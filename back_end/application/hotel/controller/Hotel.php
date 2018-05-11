7ee50068503edfe54e6f4a958621eaa0af10fb73
Yven <yvenchang@163.com>
2018-05-11 23:13:27 +0800

    fix bugs

--- back_end/application/hotel/controller/Hotel.php
+++ back_end/application/hotel/controller/Hotel.php
@@ -28,6 +28,10 @@ class Hotel extends Base
                         ->page($page, $perpage)
                         ->select();
 
+        foreach ($data as $key => $value) {
+            $value['impression'] = explode(' ', $value['impression']);
+        }
+
         if (isset($cont['position']) && preg_match('/\d+.\d,\d+.\d/', $cont['position'])) {
             $hotelPos = [];
             $distance = [];
@@ -51,6 +55,16 @@ class Hotel extends Base
 
     private function calc(Array $aDes, Array $bDes)
     {
-        return sqrt(pow((float)$aDes[0]-(float)$bDes[0], 2)+pow((float)$aDes[1]-(float)$bDes[1], 2));
+        // return sqrt(pow((float)$aDes[0]-(float)$bDes[0], 2)+pow((float)$aDes[1]-(float)$bDes[1], 2));
+        //将角度转为狐度
+        $radLat1 = deg2rad($aDes[1]);
+        //deg2rad()函数将角度转换为弧度
+        $radLat2 = deg2rad($bDes[1]);
+        $radLng1 = deg2rad($aDes[0]);
+        $radLng2 = deg2rad($bDes[0]);
+        $a = $radLat1 - $radLat2;
+        $b = $radLng1 - $radLng2;
+        $s = 2*asin(sqrt(pow(sin($a/2),2)+cos($radLat1)*cos($radLat2)*pow(sin($b/2),2)))*6371;
+        return round($s, 2);
     }
 }
